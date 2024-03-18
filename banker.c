a57a6d402a601a35b04ebbd7813d9f03368952a2
skettzu <99dwong@gmail.com>
2024-03-17 21:39:43 -0400

    Needs Matrix WIP

--- banker.c
+++ banker.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+#define NUMBER_OF_PROCESSES 5
+#define NUMBER_OF_RESOURCES 4
+
 /*
 We use the banker’s algorithm to determine whether or not a state is a safe state, and therefore, whether or not to
 grant the resource requests of the processes.
@@ -21,6 +24,16 @@ matrix, and whether or not the state is safe. If it is a safe state, output a po
 output the list of processes that cannot run. Sample output is shown on the next page – the layout of the output must
 match. Include screenshots for each input text file.
 */
+
+int available[NUMBER_OF_RESOURCES]; // the available amount of each resource
+int claim[NUMBER_OF_PROCESSES][NUMBER_OF_RESOURCES]; // the claim of each process
+int allocation[NUMBER_OF_PROCESSES][NUMBER_OF_RESOURCES]; //allocation per process
+int need[NUMBER_OF_PROCESSES][NUMBER_OF_RESOURCES]; //C – A
+
+void needs_matrix()
+{
+    // Matrix Operations to Evaluate C-A
+}
 
 int main ()
 {
